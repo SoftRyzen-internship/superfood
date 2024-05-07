68748dec3cef19dbed43eff3006442116607063e
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-07 17:35:34 +0300

    Changed  process.env.SANITY_STUDIO_PROJECT_ID

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'z0w9f3qe',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
   dataset: 'production',
   apiVersion: '2024-03-11',
   // Set to `true` for production environments

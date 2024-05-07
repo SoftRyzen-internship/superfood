1cf82326fae45b26aaffa2fbc7f10750e0eb92f2
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-07 15:00:01 +0300

    Connected Sanity.io

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import { client } from '@/utils/sanity/client';
+
+// Fetch content with GROQ
+async function getContent() {
+  const CONTENT_QUERY = `*[_type == "project"] {
+  ...,
+  coverImage {
+    ...,
+    asset->
+  },
+  duration {
+    ...
+  },
+  tags[],
+  body
+}
+`;
+  const content = await client.fetch(CONTENT_QUERY);
+  return content;
+}
+
+// Log content to console
+getContent().then(content => console.log(content));
+
+// Insert the return component calling `getContent()` below
+
 export default function Home() {
   return <main></main>;
 }

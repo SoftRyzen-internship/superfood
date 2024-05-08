848b0e97f39328a49f1ff92a46e9f48a130c3249
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-08 15:27:48 +0300

    Added data.json and styles for deck

--- src/components/ui/AdvantageItem/AdvantageItem.tsx
+++ src/components/ui/AdvantageItem/AdvantageItem.tsx
@@ -1,11 +1,12 @@
-import Done from '../../../../public/icons/Done.svg';
+import Done from '@/../public/icons/Done.svg';
 import { AdvantageItemProps } from './AdvantageItem.types';
+import item from '@/data/advantageItem.json';
 
 function AdvantageItem({ text }: AdvantageItemProps) {
   return (
-    <li className="list-none flex gap-5 items-center font-geologica fontSize-medium color-black">
-      <Done />
-      {text}
+    <li className="list-none flex gap-5 items-center font-geologica fontSize-medium font-light color-black xl:fontSize-large">
+      <Done className="w-6 h-6 xl:w-7 xl:h-7" />
+      <p>{text}</p>
     </li>
   );
 }

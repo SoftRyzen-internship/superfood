570f5f30a23146f359e86ded883f40e3b71d27ae
Violeta <vnaboishchykova@gmail.com>
2024-05-08 13:05:32 +0200

    fixed_Phone-scheme

--- sanity/schemaTypes/phoneType.ts
+++ sanity/schemaTypes/phoneType.ts
@@ -1,23 +1,43 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from 'sanity'
 
 export const phoneType = defineType({
   name: 'phone',
   title: 'Phone',
   type: 'document',
   fields: [
-            defineField({
-              name: 'country',
-              type: 'string',
-              options: {
-        list: [
-          { value: 'UA', title: 'Ukraine' },
-          { value: 'LT', title: 'Lithuania' },
-        ],
-      },
-            }),
-            defineField({
-              name: 'phone',
-              type: 'number',
-            }),
+    defineField({
+      name: 'phoneUA',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'country',
+          type: 'string',
+          readOnly: true,
+          initialValue: 'UA',
+        }),
+        defineField({
+          name: 'phone',
+          type: 'string',
+          validation: (rule) => rule.required().regex(/^\+380\d{9}$/),
+        }),
+      ],
+    }),
+    defineField({
+      name: 'phoneLT',
+      type: 'object',
+      fields: [
+        defineField({
+          name: 'country',
+          type: 'string',
+          readOnly: true,
+          initialValue: 'LT',
+        }),
+        defineField({
+          name: 'phone',
+          type: 'string',
+          validation: (rule) => rule.required().regex(/^\+370\d{9}$/),
+        }),
+      ],
+    }),
   ],
 });

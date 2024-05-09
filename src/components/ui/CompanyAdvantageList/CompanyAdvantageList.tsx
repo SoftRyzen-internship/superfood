8774042bf90089528bada146f149ca896867090c
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-09 10:51:27 +0300

    Added img @x2 for Company Advantage Section

--- src/components/ui/CompanyAdvantageList/CompanyAdvantageList.tsx
+++ src/components/ui/CompanyAdvantageList/CompanyAdvantageList.tsx
@@ -3,7 +3,7 @@ import AdvantageItem from '../AdvantageItem/AdvantageItem';
 
 function CompanyAdvantageList() {
   return (
-    <ul className="flex flex-col gap-8">
+    <ul className="flex flex-col gap-8 smOnly:mb-12">
       {data.items.map(item => {
         return <AdvantageItem key={item.id} text={item.text} />;
       })}
--- src/sections/CompanyAdvantageSection/CompanyAdvantageSection.tsx
+++ src/sections/CompanyAdvantageSection/CompanyAdvantageSection.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import CompanyAdvantageList from '@/components/ui/CompanyAdvantageList/CompanyAdvantageList';
 import data from '@/data/companyAdvantageSection.json';
 
@@ -5,10 +6,18 @@ function CompanyAdvantageSection() {
   return (
     <section className="section">
       <div className="container">
-        <h2 className="section-title text-left mb-8 xl:mb-12">
-          {data.sectionTitle}
-        </h2>
-        <CompanyAdvantageList />
+        <div>
+          <h2 className="section-title text-left mb-8 xl:mb-12">
+            {data.sectionTitle}
+          </h2>
+          <CompanyAdvantageList />
+          <Image
+            src={data.image.src}
+            width={328}
+            height={320}
+            alt={data.image.src}
+          />
+        </div>
       </div>
     </section>
   );

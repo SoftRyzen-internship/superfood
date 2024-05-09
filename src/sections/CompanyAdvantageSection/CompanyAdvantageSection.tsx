b3cf16d979ec8d552ea3d5c05e1efb4569afd1f9
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-09 11:24:40 +0300

    Added small img

--- src/sections/CompanyAdvantageSection/CompanyAdvantageSection.tsx
+++ src/sections/CompanyAdvantageSection/CompanyAdvantageSection.tsx
@@ -6,17 +6,28 @@ function CompanyAdvantageSection() {
   return (
     <section className="section">
       <div className="container">
-        <div>
-          <h2 className="section-title text-left mb-8 xl:mb-12">
-            {data.sectionTitle}
-          </h2>
-          <CompanyAdvantageList />
-          <Image
-            src={data.image.src}
-            width={328}
-            height={320}
-            alt={data.image.src}
-          />
+        <div className="flex items-center flex-col">
+          <div>
+            <h2 className="section-title text-left mb-8 xl:mb-12">
+              {data.sectionTitle}
+            </h2>
+            <CompanyAdvantageList />
+          </div>
+          <div className="relative pl-[10px] pb-[44px]">
+            <Image
+              src={data.image.src}
+              width={328}
+              height={320}
+              alt={data.image.src}
+            />
+            <Image
+              className="absolute bottom-0 left-0"
+              src={data.image.srcSmall}
+              width={148}
+              height={106}
+              alt={data.image.src}
+            />
+          </div>
         </div>
       </div>
     </section>

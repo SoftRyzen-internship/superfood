67751ee55f749a028d11ac95c221ba9e0d87f998
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-10 19:10:12 +0300

    Deleted extra div inside container

--- src/sections/CompanyAdvantageSection/CompanyAdvantageSection.tsx
+++ src/sections/CompanyAdvantageSection/CompanyAdvantageSection.tsx
@@ -5,30 +5,28 @@ import data from '@/data/companyAdvantageSection.json';
 function CompanyAdvantageSection() {
   return (
     <section className="section">
-      <div className="container">
-        <div className="flex items-center flex-col md:flex-row md:items-start">
-          <div className="md:flex-grow">
-            <h2 className="section-title text-left mb-8 xl:mb-12">
-              {data.sectionTitle}
-            </h2>
-            <CompanyAdvantageList />
-          </div>
-          <div className="relative pl-[10px] pb-[44px] md:pl-[9px] md:pb-[40px] xl:pl-[43px] xl:pb-[47px]">
-            <Image
-              className="md:w-[293px] md:h-[247px] xl:w-[592px] xl:h-[526px]"
-              src={data.image.src}
-              width={328}
-              height={320}
-              alt={data.image.src}
-            />
-            <Image
-              className="absolute bottom-0 left-0 md:w-[137px] md:h-[95px] xl:w-[277px] xl:h-[200px]"
-              src={data.image.srcSmall}
-              width={148}
-              height={106}
-              alt={data.image.src}
-            />
-          </div>
+      <div className="container flex items-center flex-col md:flex-row md:items-start">
+        <div className="md:flex-grow">
+          <h2 className="section-title text-left mb-8 xl:mb-12">
+            {data.sectionTitle}
+          </h2>
+          <CompanyAdvantageList />
+        </div>
+        <div className="relative pl-[10px] pb-[44px] md:pl-[9px] md:pb-[40px] xl:pl-[43px] xl:pb-[47px]">
+          <Image
+            className="md:w-[293px] md:h-[247px] xl:w-[592px] xl:h-[526px]"
+            src={data.image.src}
+            width={328}
+            height={320}
+            alt={data.image.src}
+          />
+          <Image
+            className="absolute bottom-0 left-0 md:w-[137px] md:h-[95px] xl:w-[277px] xl:h-[200px]"
+            src={data.image.srcSmall}
+            width={148}
+            height={106}
+            alt={data.image.src}
+          />
         </div>
       </div>
     </section>

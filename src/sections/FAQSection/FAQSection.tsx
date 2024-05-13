1668c69ec5d51bc965eaa3de0fd1c31fdd8190c9
TatianaSkl <sklyarchyk@gmail.com>
2024-05-13 12:36:10 +0300

    section faq

--- src/sections/FAQSection/FAQSection.tsx
+++ src/sections/FAQSection/FAQSection.tsx
@@ -4,12 +4,12 @@ import faq from '@/data/faqSection.json';
 
 function FAQSection() {
   return (
-    <section className="section">
-      <div className="container relative ">
+    <section className="section relative overflow-hidden">
+      <div className="container">
         <h2 className="section-title mb-8 md:mb-10 xl:mb-[55px]">{faq.title}</h2>
         <AccordionList items={faq.items} openFirst={true} />
         <Image
-          className="w-[135px] h-[135px] absolute right-[-49px] bottom-[-146px] md:w-[236px] md:h-[236px] md:right-[-75px] md:bottom-[-248px] xl:w-[322px] xl:h-[322px] xl:right-[-132px] xl:bottom-[-226px]"
+          className="w-[135px] h-[135px] absolute right-[-49px] bottom-[-86px] md:w-[236px] md:h-[236px] md:right-[-75px] md:bottom-[-168px] xl:w-[322px] xl:h-[322px] xl:right-[-132px] xl:bottom-[-106px]"
           src={faq.src}
           width={322}
           height={322}

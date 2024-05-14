4d367a6c46d962d0002af95d0a10b2cfc4b89004
TatianaSkl <sklyarchyk@gmail.com>
2024-05-14 14:47:10 +0300

    route

---
+++ src/app/[product]/page.tsx
@@ -0,0 +1,11 @@
+// export async function generateStaticParams() {
+//   const posts = await fetch('https://.../posts').then(res => res.json());
+
+//   return posts.map(post => ({
+//     slug: post.slug,
+//   }));
+// }
+
+// export default function Page({ params }: { params: { slug: string } }) {
+//   return <div>My Post: {params.slug}</div>;
+// }

cffa3b372c669d9e7583e51afde6a5cb3292afa1
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-05-14 13:58:26 +0300

    Update ProductAction.tsx

--- src/components/ui/ProductAction/ProductAction.tsx
+++ src/components/ui/ProductAction/ProductAction.tsx
@@ -5,7 +5,7 @@ function ProductAction({ data }: { data: DataProps }) {
   return (
     <ul className='flex gap-2 xl:gap-3 bg-transparent'>
       {data.productAction.map(product => (
-        <li key={product.id} className='font-geologica font-light text-black border border-strokeGreen rounded-lg	 py-2 px-3 text-[14px] leading-[18.9px] xl:text-[18px] xl:leading-[24.3px] transition'>
+        <li key={product.id} className='font-geologica font-light text-black border border-strokeGreen rounded-lg	 py-2 px-3 text-[14px] leading-[18.9px]  transition  xl:font-[250] xl:text-[16] xl:leading-[22.4px]'>
           {product.action}
         </li>
       ))}

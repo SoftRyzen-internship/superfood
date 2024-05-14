ef00587cc36aa57b31d0746bc663d9405f27ce23
TatianaSkl <sklyarchyk@gmail.com>
2024-05-14 13:17:37 +0300

    fix prop

--- src/sections/FAQSection/FAQSection.tsx
+++ src/sections/FAQSection/FAQSection.tsx
@@ -7,7 +7,7 @@ function FAQSection() {
     <section className="section overflow-x-clip">
       <div className="container relative">
         <h2 className="section-title mb-8 md:mb-10 xl:mb-[55px]">{faq.title}</h2>
-        <AccordionList items={faq.items} />
+        <AccordionList items={faq.items} openFirst={true} />
         <Image
           className="w-[135px] h-[135px] absolute right-[-49px] bottom-[-146px] md:w-[236px] md:h-[236px] md:right-[-75px] md:bottom-[-248px] xl:w-[322px] xl:h-[322px] xl:right-[-179px] xl:bottom-[-226px]"
           src={faq.src}

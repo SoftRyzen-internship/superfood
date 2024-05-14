272ac8f1e2a92221645fa1ec159d969e7f878334
TatianaSkl <sklyarchyk@gmail.com>
2024-05-14 10:55:25 +0300

    fix image

--- src/sections/FAQSection/FAQSection.tsx
+++ src/sections/FAQSection/FAQSection.tsx
@@ -4,12 +4,13 @@ import faq from '@/data/faqSection.json';
 
 function FAQSection() {
   return (
-    <section className="section relative overflow-x-clip">
-      <div className="container">
+    <section className="section overflow-x-clip">
+      <div className="container relative">
         <h2 className="section-title mb-8 md:mb-10 xl:mb-[55px]">{faq.title}</h2>
         <AccordionList items={faq.items} openFirst={true} />
+
         <Image
-          className="w-[135px] h-[135px] absolute right-[-49px] bottom-[-86px] md:w-[236px] md:h-[236px] md:right-[-75px] md:bottom-[-168px] xl:w-[322px] xl:h-[322px] xl:right-[-132px] xl:bottom-[-106px]"
+          className="w-[135px] h-[135px] absolute right-[-49px] bottom-[-146px] md:w-[236px] md:h-[236px] md:right-[-75px] md:bottom-[-248px] xl:w-[322px] xl:h-[322px] xl:right-[-179px] xl:bottom-[-226px]"
           src={faq.src}
           width={322}
           height={322}

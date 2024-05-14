710d7978a0ca2ac7ba487d1eea68b0af3c7535f4
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-05-14 18:33:58 +0300

    add section action and preference and styles

--- src/components/ui/ProductAction/ProductAction.tsx
+++ src/components/ui/ProductAction/ProductAction.tsx
@@ -3,7 +3,7 @@ import { DataProps } from './ProductAction.types';
 
 function ProductAction({ data }: { data: DataProps }) {
   return (
-    <ul className='flex gap-2 xl:gap-3 bg-transparent'>
+    <ul className='flex flex-wrap	gap-2 xl:gap-3 bg-transparent'>
       {data.productAction.map(product => (
         <li key={product.id} className='font-geologica font-light text-black border border-strokeGreen rounded-lg	 py-2 px-3 text-[14px] leading-[18.9px]  transition  xl:font-[250] xl:text-[16] xl:leading-[22.4px]'>
           {product.action}

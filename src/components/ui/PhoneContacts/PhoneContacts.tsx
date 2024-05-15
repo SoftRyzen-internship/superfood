c34a804163c479089751af713826b086e5ea7ee8
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-05-15 11:35:17 +0300

    change footer hover colors

--- src/components/ui/PhoneContacts/PhoneContacts.tsx
+++ src/components/ui/PhoneContacts/PhoneContacts.tsx
@@ -7,7 +7,7 @@ function PhoneContacts({ location, classNames: additionalClasses }: PhoneProps) 
   const PhoneStyles =
     location === 'contacts'
       ? 'font-geologica text-black font-light text-[18px] leading-[24.3px]  md:text-[20px] md:leading-[27px] xl:text-[26px] xl:leading-[35.1px] hover:text-green'
-      : 'font-geologica text-lightGreen font-extralight text-[14px] leading-[18.9px] text-center md:font-[250] md:text-[16px] md:leading-[22.4px] hover:font-light hover:text-white';
+      : 'font-geologica text-lightGreen font-extralight text-[14px] leading-[18.9px] text-center md:font-[250] md:text-[16px] md:leading-[22.4px] hover:text-white';
 
   const PhoneClasses = classNames(
     'cursor-pointer inline-block transition tracking-[-0.02em] flex',

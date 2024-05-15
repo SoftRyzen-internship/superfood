9ba842dad81cf13e400a20fe61109e44bfa9b560
TatianaSkl <sklyarchyk@gmail.com>
2024-05-15 12:42:32 +0300

    added policy section

--- src/app/(site)/policy/page.tsx
+++ src/app/(site)/policy/page.tsx
@@ -1,5 +1,12 @@
+'use client';
+
+import { getPolicy } from '../../../../sanity/requests';
+import PolicySection from '@/sections/PolicySection';
+
+getPolicy().then(content => console.log(content));
+
 function PageSample() {
-  return <div className="container">PolicyPage</div>;
+  return <PolicySection />;
 }
 
 export default PageSample;

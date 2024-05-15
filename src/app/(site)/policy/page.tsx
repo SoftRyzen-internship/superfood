3e90101cf46e9ace56653d9281219dc432641f8b
TatianaSkl <sklyarchyk@gmail.com>
2024-05-15 15:19:55 +0300

    added getPolicy

--- src/app/(site)/policy/page.tsx
+++ src/app/(site)/policy/page.tsx
@@ -1,12 +1,20 @@
 'use client';
-
+import React, { useState, useEffect } from 'react';
 import { getPolicy } from '../../../../sanity/requests';
 import PolicySection from '@/sections/PolicySection';
+import { PolicyContent } from '@/sections/PolicySection/PolicySection.types';
 
-getPolicy().then(content => console.log(content));
+function Policy() {
+  const [policyContent, setPolicyContent] = useState<PolicyContent | null>(null);
 
-function PageSample() {
-  return <PolicySection />;
+  useEffect(() => {
+    getPolicy().then(content => {
+      console.log(content[0]);
+      setPolicyContent(content[0]);
+    });
+  }, []);
+
+  return <PolicySection content={policyContent} />;
 }
 
-export default PageSample;
+export default Policy;
--- src/sections/PolicySection/PolicySection.tsx
+++ src/sections/PolicySection/PolicySection.tsx
@@ -1,27 +1,49 @@
-function PolicySection() {
+import { PolicyContentProps } from './PolicySection.types';
+
+function PolicySection({ content }: PolicyContentProps) {
   return (
     <section className="pt-[140px] pb-20 md:pt-[192px] md:pb-[120px] xl:pt-[212px]">
       <div className="container md:px-[70px] xl:px-[136px]">
-        <h1 className="font-roboto text-green text-[18px] font-bold leading-[1.30] uppercase mb-10 md:text-[24px] md:leading-[1.15] md:text-center md:mb-14 md:w-[580px] xl:w-full xl:text-[30px] xl:mb-16">
-          ПОЛІТИКА КОНФІДЕНЦІЙНОСТІ ТА ЗАХИСТУ ПЕРСОНАЛЬНИХ ДАНИХ
-        </h1>
-        <p className="font-geologica text-textBody font-light text-[14px] leading-[1.35] tracking-[-0.28px] mb-8 md:text-[16px] md:tracking-[-0.32px] md:mb-10 xl:text-[18px] xl:tracking-[-0.36px]">
-          Управління сайтом https://........... (далі – Сайт) здійснюється адміністратором сайту
-          (далі за текстом разом іменуються – Виконавець). Ця Політика конфіденційності та захисту
-          персональних даних (надалі – «Політика») є невід’ємною частиною публічного договору
-          (публічної оферти)  У цій Політиці визначено порядок здійснення Виконавцем оброблення
-          персональних даних, види персональних даних, які збираються, цілі використання таких
-          персональних даних, взаємодію Виконавця з третіми особами, заходи безпеки для захисту
-          персональних даних, умови доступу до персональних даних, а також контактну інформацію для
-          користувача щодо отримання доступу, внесення змін, блокування або видалення своїх
-          персональних даних та звернення за будь-якими питаннями, які можуть виникнути у Замовника
-          щодо практики захисту персональних даних.
-        </p>
-        <h2 className="font-roboto text-green text-[14px] font-bold leading-[1.15] uppercase mb-4 md:text-[16px] md:leading-[1.30] xl:text-[20px] xl:mb-6">
-          Збір та використання персональних даних
-        </h2>
+        {content ? (
+          <div key={content._id}>
+            <h1 className="font-roboto text-green text-[18px] font-bold leading-[1.30] uppercase mb-10 md:text-[24px] md:leading-[1.15] md:text-center md:mb-14 md:w-[580px] xl:w-full xl:text-[30px] xl:mb-16">
+              {content.title}
+            </h1>
+            {content.description.map((item, index) => (
+              <div key={index}>
+                {item.children.map((child, idx) => (
+                  <p key={idx}>{child.text}</p>
+                ))}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </section>
+    // <section className="pt-[140px] pb-20 md:pt-[192px] md:pb-[120px] xl:pt-[212px]">
+    //   <div className="container md:px-[70px] xl:px-[136px]">
+    //     <h1 className="font-roboto text-green text-[18px] font-bold leading-[1.30] uppercase mb-10 md:text-[24px] md:leading-[1.15] md:text-center md:mb-14 md:w-[580px] xl:w-full xl:text-[30px] xl:mb-16">
+    //       ПОЛІТИКА КОНФІДЕНЦІЙНОСТІ ТА ЗАХИСТУ ПЕРСОНАЛЬНИХ ДАНИХ
+    //     </h1>
+    //     <p className="font-geologica text-textBody font-light text-[14px] leading-[1.35] tracking-[-0.28px] mb-8 md:text-[16px] md:tracking-[-0.32px] md:mb-10 xl:text-[18px] xl:tracking-[-0.36px]">
+    //       Управління сайтом https://........... (далі – Сайт) здійснюється адміністратором сайту
+    //       (далі за текстом разом іменуються – Виконавець). Ця Політика конфіденційності та захисту
+    //       персональних даних (надалі – «Політика») є невід’ємною частиною публічного договору
+    //       (публічної оферти)  У цій Політиці визначено порядок здійснення Виконавцем оброблення
+    //       персональних даних, види персональних даних, які збираються, цілі використання таких
+    //       персональних даних, взаємодію Виконавця з третіми особами, заходи безпеки для захисту
+    //       персональних даних, умови доступу до персональних даних, а також контактну інформацію для
+    //       користувача щодо отримання доступу, внесення змін, блокування або видалення своїх
+    //       персональних даних та звернення за будь-якими питаннями, які можуть виникнути у Замовника
+    //       щодо практики захисту персональних даних.
+    //     </p>
+    //     <h2 className="font-roboto text-green text-[14px] font-bold leading-[1.15] uppercase mb-4 md:text-[16px] md:leading-[1.30] xl:text-[20px] xl:mb-6">
+    //       Збір та використання персональних даних
+    //     </h2>
+    //   </div>
+    // </section>
   );
 }
 

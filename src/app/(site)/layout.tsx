bdf7f89e05729bb2ada58e4be9aa4721f01d9582
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-16 13:45:34 +0300

    Updated og-image

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     siteName: 'СУПЕРФУДИ',
     images: [
       {
-        url: 'https://github.com/darynakarmazin/carp-travel/raw/main/src/og-image/og-image.png',
+        url: '/og-image/og-image.jpg',
         width: 1200,
         height: 630,
         alt: 'СУПЕРФУДИ',

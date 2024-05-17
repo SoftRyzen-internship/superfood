0e44084874cd075ce42de99a8194d7e70ddf2242
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-17 11:52:56 +0300

    Deleted console.logs

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,34 +1,19 @@
-import {
-  getPolicy,
-  getPhones,
-  getProduct,
-  getReviews,
-} from '../../../sanity/requests';
-
 import CompanyAdvantageSection from '@/sections/CompanyAdvantageSection/CompanyAdvantageSection';
 import HeroSection from '@/sections/HeroSection';
-import ProductCard from '@/components/ui/ProductCard';
 import FAQSection from '@/sections/FAQSection';
 import Benefits from '@/sections/Benefits/Benefits';
 import Contacts from '@/sections/Contacts';
 import Reviews from '@/sections/Reviews/Reviews';
 import Products from '@/sections/Products';
 
-// Log content to console
-// getPolicy().then(content => console.log(content[0].description[0].children));
-// getPhones().then(content => console.log(content));
-getProduct().then(content => console.log(content));
-// getReviews().then(content => console.log(content));
-
 export default function Home() {
-  const productId = 2;
   return (
     <main>
       <HeroSection />
       <Benefits />
       <Products />
       <CompanyAdvantageSection />
-      <Reviews/>
+      <Reviews />
       <FAQSection />
       <Contacts />
     </main>

e202207e711ee5abb0fdd5048d98c73a03dae95f
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-05-21 16:56:40 +0300

    fix some bugs

--- src/components/ui/ProductAction/ProductAction.tsx
+++ src/components/ui/ProductAction/ProductAction.tsx
@@ -3,11 +3,11 @@ import { ActionProps } from './ProductAction.types';
 
 function ProductAction({ actions }: ActionProps) {
   return (
-    <ul className="flex flex-wrap gap-2 xl:gap-3 bg-transparent">
+    <ul className="flex flex-wrap gap-2 bg-transparent md:w-[615px]">
       {actions.map((action, index) => (
         <li
           key={index}
-          className="font-geologica font-light text-black border border-strokeGreen rounded-lg py-2 px-3 text-[14px] leading-[18.9px] transition smOnly:w-full xl:font-[250] xl:text-[16] xl:leading-[22.4px]"
+          className="font-geologica font-light text-black border border-strokeGreen rounded-lg py-2 px-3 text-[14px] leading-[18.9px] transition smOnly:w-full xl:font-[250] xl:text-[16px] xl:leading-[22.4px]"
         >
           {action}
         </li>
